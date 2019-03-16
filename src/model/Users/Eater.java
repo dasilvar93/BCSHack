eca6fdbef6baa2340ead26c7d923b2d388bfa40e
Ricky <rickycurry@gmail.com>
2019-03-16 12:17:16 -0700

    Added constructors for eater and grower

--- src/model/Users/Eater.java
+++ src/model/Users/Eater.java
@@ -1,4 +1,10 @@
 package model.Users;
 
+import model.City;
+
 public class Eater extends User {
+
+    public Eater(Double longitude, Double latitude, String name, City city) {
+        super(longitude, latitude, name, city);
+    }
 }
--- src/model/Users/Grower.java
+++ src/model/Users/Grower.java
@@ -1,6 +1,11 @@
 package model.Users;
 
+import model.City;
+
 public class Grower extends User {
 
+    public Grower(Double longitude, Double latitude, String name, City city) {
+        super(longitude, latitude, name, city);
+    }
 
 }
