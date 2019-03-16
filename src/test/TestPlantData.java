9df9383aa735080dd74148d9696727a2862e0f2a
Ricky <rickycurry@gmail.com>
2019-03-16 15:02:26 -0700

    Something didn't work so let's tryyyy again

--- src/test/TestPlantData.java
+++ src/test/TestPlantData.java
@@ -3,24 +3,17 @@ package test;
 import model.Plant;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
-import static parsers.PlantsParser.jsonToPlants;
+import static parsers.PlantsParser.parseDefault;
 
 public class TestPlantData {
 
     @Test
     public void testPlantParsing() {
-        String plantDataPath = "./src/data/appletrees.json";
-        try {
-            String plantsJson = new String(Files.readAllBytes(Paths.get(plantDataPath)));
-            List<Plant> plants = jsonToPlants(plantsJson);
-            System.out.println(plants.get(0).getSpecies());
-        } catch (Exception e) {
-            System.out.println(e);
-            System.out.println("huge failure!");
-        }
+        List<Plant> trees = parseDefault();
+        System.out.println(trees.get(0).getSpecies());
+        System.out.println(trees.get(0).getLong());
+        System.out.println(trees.get(0).getLat());
     }
 }
