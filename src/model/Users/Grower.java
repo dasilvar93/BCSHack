1d43d2b418091d4228e90f296814c243e1b5e9e3
dasilvar93 <kadasilva22@gmail.com>
2019-03-16 12:39:47 -0700

    More implementation

--- src/model/Users/Grower.java
+++ src/model/Users/Grower.java
@@ -4,9 +4,6 @@ import model.City;
 
 public class Grower extends User {
 
-    public Grower(Double longitude, Double latitude, String name, City city) {
-        super(longitude, latitude, name, city);
-    }
 
     public Grower() {
         super();
