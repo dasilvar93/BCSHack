67408078adc8b89b856c7d7d35fa9a5a2226b59c
MayKikuchi <m.may.kikuchi@gmail.com>
2019-03-16 15:49:53 -0700

    made more pannel stuff

--- src/test/TestPlantData.java
+++ src/test/TestPlantData.java
@@ -1,26 +1,26 @@
-package test;
-
-import model.Plant;
-import org.junit.jupiter.api.Test;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
-import static parsers.PlantsParser.jsonToPlants;
-
-public class TestPlantData {
-
-    @Test
-    public void testPlantParsing() {
-        String plantDataPath = "./src/data/appletrees.json";
-        try {
-            String plantsJson = new String(Files.readAllBytes(Paths.get(plantDataPath)));
-            List<Plant> plants = jsonToPlants(plantsJson);
-            System.out.println(plants.get(0).getSpecies());
-        } catch (Exception e) {
-            System.out.println(e);
-            System.out.println("huge failure!");
-        }
-    }
-}
+//package test;
+//
+//import model.Plant;
+//import org.junit.jupiter.api.Test;
+//
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.List;
+//
+//import static parsers.PlantsParser.jsonToPlants;
+//
+//public class TestPlantData {
+//
+//    @Test
+//    public void testPlantParsing() {
+//        String plantDataPath = "./src/data/appletrees.json";
+//        try {
+//            String plantsJson = new String(Files.readAllBytes(Paths.get(plantDataPath)));
+//            List<Plant> plants = jsonToPlants(plantsJson);
+//            System.out.println(plants.get(0).getSpecies());
+//        } catch (Exception e) {
+//            System.out.println(e);
+//            System.out.println("huge failure!");
+//        }
+//    }
+//}
