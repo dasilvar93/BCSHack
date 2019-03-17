a53246feecc16f8fdc1cd7efa4d6ea1b9d2dd513
Ricky <rickycurry@gmail.com>
2019-03-16 17:06:40 -0700

    a

--- src/test/CityTest.java
+++ src/test/CityTest.java
@@ -3,16 +3,43 @@ package test;
 import model.City;
 import model.Plant;
 import model.Users.Eater;
+import model.Users.Grower;
 import model.Users.User;
+import org.junit.jupiter.api.Test;
 import parsers.PlantsParser;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class CityTest {
 
-    List<Plant> plants = PlantsParser.parseDefault();
-    City vancouver = new City("Vancouver", 7);
-    User user1 = new Eater(49.261210, -123.248383, "Bob", vancouver);
-    User user2 = new Eater(49.262210, -123.247383, "Jim", vancouver);
+    private List<Plant> plants = PlantsParser.parseDefault();
+    private City vancouver = new City("Vancouver", 7);
+    private User user1 = new Eater(49.261210, -123.248383, "Bob", vancouver);
+    private User user2 = new Eater(49.262210, -123.247383, "Jim", vancouver);
+    private User user3 = new Grower(49.261210, -123.248383, "Jane", vancouver);
+    private User user4 = new Grower(49.262210, -123.247383, "Ellie", vancouver);
+
+    @Test
+    public void testAddPlantsToCity() {
+        for (Plant p : plants) {
+            vancouver.addPlant(p);
+        }
+        Plant plant = plants.get(0);
+        plant.changeHarvestable();
+        plant.readyToHarvest();
+    }
+
+    @Test
+    public void testNotify() {
+        List<User> users = vancouver.getUsers();
+        assertFalse(plants.isEmpty());
+        assertFalse(users.isEmpty());
+        for (User u : users) {
+            u.notify(plants.get(0), "harvest");
+            u.notify(plants.get(0), "water");
+        }
+    }
 
 }
--- src/test/GeneratePlantData.java
+++ src/test/GeneratePlantData.java
@@ -1,45 +1,45 @@
-//package test;
-//
-//import model.Plant;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.FileWriter;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static java.lang.Math.abs;
-//import static java.lang.Math.random;
-//import static persistance.Jsonifier.plantsToJson;
-//
-//
-//public class GeneratePlantData {
-//
-//    private String folder = "./src/data/";
-//    private double centralLong = 49.261210;
-//    private double centralLat = -123.248383;
-//    private double cornerLong = 49.256268;
-//    private double cornerLat = -123.255120;
-//    private double maxLongVar = abs(centralLong - cornerLong);
-//    private double maxLatVar = abs(centralLat - cornerLat);
-//
-//    @Test
-//    public void generateTestData() {
-//        String fileName = "appletrees.json";
-//        List<Plant> trees = new ArrayList<>();
-//        for (int i = 0; i < 15; i++) {
-//            trees.add(new Plant("apple", (centralLong + (random()-0.5) * 2 * maxLongVar),
-//                    centralLat + ((random()-0.5) * 2 * maxLatVar)));
-//        }
-//        for (int i = 0; i < 15; i++) {
-//            trees.add(new Plant("pear", (centralLong + (random()-0.5) * 2 * maxLongVar),
-//                    centralLat + ((random()-0.5) * 2 * maxLatVar)));
-//        }
-//        try {
-//            FileWriter file = new FileWriter(folder + fileName);
-//            file.write(plantsToJson(trees).toString());
-//            file.close();
-//        } catch (Exception e) {
-//            System.out.println("JSON write failed.");
-//        }
-//    }
-//}
+package test;
+
+import model.Plant;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.random;
+import static persistance.Jsonifier.plantsToJson;
+
+
+public class GeneratePlantData {
+
+    private String folder = "./src/data/";
+    private double centralLong = 49.261210;
+    private double centralLat = -123.248383;
+    private double cornerLong = 49.256268;
+    private double cornerLat = -123.255120;
+    private double maxLongVar = abs(centralLong - cornerLong);
+    private double maxLatVar = abs(centralLat - cornerLat);
+
+    @Test
+    public void generateTestData() {
+        String fileName = "appletrees.json";
+        List<Plant> trees = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            trees.add(new Plant("apple", (centralLong + (random()-0.5) * 2 * maxLongVar),
+                    centralLat + ((random()-0.5) * 2 * maxLatVar)));
+        }
+        for (int i = 0; i < 15; i++) {
+            trees.add(new Plant("pear", (centralLong + (random()-0.5) * 2 * maxLongVar),
+                    centralLat + ((random()-0.5) * 2 * maxLatVar)));
+        }
+        try {
+            FileWriter file = new FileWriter(folder + fileName);
+            file.write(plantsToJson(trees).toString());
+            file.close();
+        } catch (Exception e) {
+            System.out.println("JSON write failed.");
+        }
+    }
+}
